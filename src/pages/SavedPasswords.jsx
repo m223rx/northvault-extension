9515354ce3ce7ff93e1a47cb05b97f83f56d6852
m223rx <127022843+m223rx@users.noreply.github.com>
2025-10-25 23:41:36 +0100

    latest update

--- src/pages/SavedPasswords.jsx
+++ src/pages/SavedPasswords.jsx
@@ -23,7 +23,7 @@ export default function SavedPasswords({ setCurrentView }) {
                     {data.map((item, index) => (
                         <div
                             key={index}
-                            onClick={() => setIsShown(!isShown)} 
+                            onClick={() => setIsShown(!isShown)}
                             className='w-full h-15 border border-[#696FC7] rounded-lg flex flex-row items-center justify-between gap-1 cursor-pointer'>
                             <div className='w-20 h-full flex items-center justify-center bg-[#696FC7] rounded-tl-lg rounded-bl-lg'>
                                 <item.icon className='h-7 w-7 text-[#fff]' />
@@ -43,23 +43,29 @@ export default function SavedPasswords({ setCurrentView }) {
                 {isShown && (
                     <div className='inset-0 absolute h-full w-[100%] bg-black/20 justify-center flex flex-col px-5'>
                         <div className='flex flex-col bg-white h-50 px-5 py-5 justify-center shadow-lg  rounded-lg relative gap-2 border border-[#696FC7]'>
-                            <label htmlFor="password_length" className='text-sm font-semibold text-[#696FC7]'>Password</label>
-                            <div className='relative border p-2 rounded-lg border-[#696FC7] flex flex-row'>
-                                <input type="text" placeholder='**********' className='outline-none' />
-                                <button className='absolute top-0 right-0 px-2 bg-[#696FC7] h-full rounded-tr-lg rounded-br-lg'>
-                                    {isVisible ? (
-                                        <EyeOff className='text-[#fff]' />
-                                    ) : (
-                                        <Eye className='text-[#fff]' />
-                                    )}
+                            <div className='flex flex-col gap-2 justify-center'>
+                                <label htmlFor="password_length" className='text-sm font-semibold text-[#696FC7]'>Password</label>
+                                <div className='relative border p-2 rounded-lg border-[#696FC7] flex flex-row'>
+                                    <input type={`${isVisible ? 'text' : 'password'}`} disabled placeholder='**********' className='w-full outline-none' />
+                                    <button className='absolute top-0 right-0 px-2 bg-[#696FC7] h-full rounded-tr-lg rounded-br-lg cursor-pointer' onClick={() => setIsVisible(!isVisible)} >
+                                        {isVisible ? (
+                                            <EyeOff className='text-[#fff]' />
+                                        ) : (
+                                            <Eye className='text-[#fff]' />
+                                        )}
+                                    </button>
 
-                                </button>
+                                </div>
+                                <div className='flex items-center justify-center'>
+                                    <button className='bg-[#696FC7] w-full px-2 py-2 mt-2 rounded-lg text-[#fff] font-bold cursor-pointer'>Copy</button>
+                                </div>
+                                <div className='absolute top-0 right-0'>
+                                    <button className='bg-[#ff0000] px-2 py-2 rounded-tr-lg cursor-pointer rounded-bl-lg'>
+                                        <X className='text-[#fff] font-bold' onClick={() => setIsShown(!isShown)} />
+                                    </button>
+                                </div>
                             </div>
-                            <div className='absolute top-0 right-0'>
-                                <button className='bg-[#ff0000] px-2 py-2 rounded-tr-lg cursor-pointer rounded-bl-lg'>
-                                    <X className='text-[#fff] font-bold' onClick={() => setIsShown(!isShown)} />
-                                </button>
-                            </div>
+
                         </div>
                     </div>
                 )}
